e23b87620f9bb09f9306405f000ea60e0ce151dc
Summersby95 <james.summersby@gmail.com>
2021-12-07 15:28:54 +0000

    feat: add urls for creating, adding products/commercial details

--- npd/products/urls.py
+++ npd/products/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='product_home'),
     path('<int:product_id>/', views.product_details, name='product_details'),
+    path('create/', views.create_product, name='create_product'),
+    path('<int:product_id>/commercial/', views.add_commercial_details, name='add_commercial_details'),
+    path('<int:product_id>/commercial/edit', views.edit_commercial_details, name='edit_commercial_details'),
 ]
