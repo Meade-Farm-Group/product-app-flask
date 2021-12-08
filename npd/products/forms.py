522f52838a5812479a3e3af93a84634282539e2b
Summersby95 <james.summersby@gmail.com>
2021-12-08 15:25:25 +0000

    feat: create forms for packaging and operations models

--- npd/products/forms.py
+++ npd/products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, CommercialModel
+from .models import OperationsModel, PackagingModel, Product, CommercialModel
 from ancillaries.forms import DateInput
 
 
@@ -25,4 +25,33 @@ class CommercialForm(forms.ModelForm):
         ]
         widgets = {
             'start_date': DateInput(),
+        }
+
+
+class PackagingForm(forms.ModelForm):
+    """ Packaging Form """
+    class Meta:
+        model = PackagingModel
+        exclude = [
+            'product',
+            'created_on',
+            'created_by',
+        ]
+        widgets = {
+            'date_ordered': DateInput(),
+            'delivery_date': DateInput(),
+        }
+
+
+class OperationsForm(forms.ModelForm):
+    """ Operations Form """
+    class Meta:
+        model = OperationsModel
+        exclude = [
+            'product',
+            'created_on',
+            'created_by',
+        ]
+        widgets = {
+            'test_date': DateInput(),
         }
