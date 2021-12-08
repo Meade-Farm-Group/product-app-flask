b25e4666efa5e877165a7815303068fd3eb70eef
Summersby95 <james.summersby@gmail.com>
2021-12-08 15:26:24 +0000

    feat: create url patterns for forms

--- npd/products/urls.py
+++ npd/products/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('create/', views.create_product, name='create_product'),
     path('<int:product_id>/commercial/', views.add_commercial_details, name='add_commercial_details'),
     path('<int:product_id>/commercial/edit', views.edit_commercial_details, name='edit_commercial_details'),
+    path('<int:product_id>/packaging/', views.add_packaging_details, name="add_packaging_details"),
+    path('<int:product_id>/packaging/edit', views.edit_packaging_details, name='edit_packaging_details'),
+    path('<int:product_id>/operations/', views.add_operations_details, name='add_operations_details'),
+    path('<int:product_id>/operations/edit', views.edit_operations_details, name='edit_operations_details'),
+    path('<int:product_id>/edit/', views.edit_navigation, name='edit_navigation'),
 ]
