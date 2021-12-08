dd11a031284a347552b3e9292da86c79f0e438d7
Summersby95 <james.summersby@gmail.com>
2021-12-08 15:29:13 +0000

    feat: create admin models for packaging and operations

--- npd/products/admin.py
+++ npd/products/admin.py
@@ -24,8 +24,26 @@ class CommercialAdmin(admin.ModelAdmin):
     )
 
 
+class PackagingAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'created_on',
+        'created_by',
+    )
+
+
+class OperationsAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'created_on',
+        'created_by',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductStatus)
 admin.site.register(Variety)
 admin.site.register(Origin)
 admin.site.register(CommercialModel, CommercialAdmin)
+admin.site.register(PackagingModel, PackagingAdmin)
+admin.site.register(OperationsModel, OperationsAdmin)
