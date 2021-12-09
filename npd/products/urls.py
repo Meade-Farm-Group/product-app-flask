7575d868d41f94044c8619981f3e25b99c95caea
Summersby95 <james.summersby@gmail.com>
2021-12-09 15:27:59 +0000

    feat: add urls for technical details editing

--- npd/products/urls.py
+++ npd/products/urls.py
@@ -12,4 +12,11 @@ urlpatterns = [
     path('<int:product_id>/operations/', views.add_operations_details, name='add_operations_details'),
     path('<int:product_id>/operations/edit', views.edit_operations_details, name='edit_operations_details'),
     path('<int:product_id>/edit/', views.edit_navigation, name='edit_navigation'),
+    path('<int:product_id>/technical/', views.technical_navigation, name='technical_navigation'),
+    path('<int:product_id>/technical/finished/', views.add_finished_specification, name='add_finished_specification'),
+    path('<int:product_id>/technical/finished/edit', views.edit_finished_specification, name='edit_finished_specification'),
+    path('<int:product_id>/technical/defect/', views.add_defect_specification, name='add_defect_specification'),
+    path('<int:product_id>/technical/defect/<int:defect_spec_id>/edit', views.edit_defect_specification, name='edit_defect_specification'),
+    path('<int:product_id>/technical/defect/view', views.product_defects, name='product_defects'),
+    path('<int:product_id>/technical/defect/<int:defect_spec_id>/delete', views.delete_defect_specification, name='delete_defect_specification'),
 ]
