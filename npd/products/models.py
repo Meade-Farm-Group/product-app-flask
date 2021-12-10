806a1126560634fe953918cc733baf78a3fedbd6
Summersby95 <james.summersby@gmail.com>
2021-12-10 08:42:00 +0000

    fix: imports for models

--- npd/products/models.py
+++ npd/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ancillaries.models import Customer, Department, Supplier
+from ancillaries.models import Customer, Department, Supplier, Defect
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
