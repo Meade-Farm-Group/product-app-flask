a1533d374ffeb3370743e13cc58c126a322577df
Summersby95 <james.summersby@gmail.com>
2022-01-19 14:56:26 +0000

    feat: add email notification for new products

--- npd/products/signals.py
+++ npd/products/signals.py
@@ -1,13 +1,35 @@
+import os
 from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.shortcuts import reverse
+from .views import product_details
+from django.db.models import Q
 from django.dispatch import receiver
+from django.core.mail import send_mail
 from .models import *
+from users.models import Profile
+
+
+def created_email(user_list, product, link):
+    send_mail(
+        subject=f'New Product Created: {product.product_name}',
+        message=f'',
+        recipient_list=user_list,
+        from_email=os.environ.get('EMAIL_HOST_USER', ''),
+        html_message=f"""
+            <h1>New Product: {product.product_name}</h1>
+            <h3><b>Customer:</b> {product.customer}</h3>
+            <h3><b>Department:</b> {product.department}</h3>
+            <h3><b>Start Date:</b> {product.start_date}</h3>
+            <h3><b>Link:</b> {link}</h3>
+        """
+    )
 
 
 @receiver(post_save, sender=Product)
 def product_notification(sender, instance, created, **kwargs):
     if created:
-        # send email of new product created
-        pass
+        user_list = User.objects.values_list('email', flat=True)
+        created_email(user_list, instance, reverse(product_details, args=[instance.id]))
     else:
         # send email of product updated
         pass
