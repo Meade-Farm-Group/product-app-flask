b3e6994f1747f1dfe1701a6635424eb993c38c9c
Summersby95 <james.summersby@gmail.com>
2022-01-24 13:11:24 +0000

    feat: create view for user tasks

--- npd/users/views.py
+++ npd/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-# Create your views here.
+from products.models import Product, ProductStatus
+
+@login_required
+def user_tasks(request):
+    group = request.user.groups.first()
+    outstanding_products = Product.objects.filter(
+        ~Q(status=ProductStatus.objects.get(status="Completed - Production Ready"))
+    )
+    user_products = []
+    for product in outstanding_products:
+        if group.name in product.outstanding_checks():
+            user_products.append(product)
+
+    return render(request, 'users/user_tasks.html', {
+        'user_products': user_products,
+    })
