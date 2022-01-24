d6bc4ba41fb4a61a34d05b184d8a53926eac0379
Summersby95 <james.summersby@gmail.com>
2022-01-24 13:05:35 +0000

    feat: add forms for ancillaries app

--- npd/ancillaries/forms.py
+++ npd/ancillaries/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 
+from .models import Customer, Defect
+
+
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+
 class TimeInput(forms.TimeInput):
     input_type = 'time'
+
+
+class DefectForm(forms.ModelForm):
+    class Meta:
+        model = Defect
+        fields = [
+            'defect_name',
+        ]
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = [
+            'customer_name',
+        ]
