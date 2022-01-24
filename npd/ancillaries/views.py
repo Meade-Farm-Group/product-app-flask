4d47e2d61262d837affd48297012e2a48bec634f
Summersby95 <james.summersby@gmail.com>
2022-01-24 13:06:03 +0000

    feat: add views to view, add, delete defects

--- npd/ancillaries/views.py
+++ npd/ancillaries/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect, get_object_or_404, reverse
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+
+from .models import Customer, Defect
+from .forms import DefectForm, CustomerForm
+
+
+@login_required
+@permission_required('products.add_finishedproduct', raise_exception=True)
+def view_defects(request):
+    defects = Defect.objects.all()
+    
+    return render(request, 'ancillaries/defect_table.html', {
+        'defects': defects,
+    })
+
+
+@login_required
+@permission_required('products.add_finishedproduct', raise_exception=True)
+def add_defect(request):
+    if request.method == 'POST':
+        form = DefectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Defect Created Successfully!")
+            return redirect(reverse(view_defects))
+        else:
+            messages.error(request, "Error Creating Defect! Please Try Again!")
+    else:
+        form = DefectForm()
+
+    return render(request, 'ancillaries/defect_form.html', {
+        'form': form
+    })
+
+
+@login_required
+@permission_required('product.add_finishedproduct', raise_exception=True)
+def delete_defect(request, defect_id):
+    defect = get_object_or_404(Defect, pk=defect_id)
+
+    if request.method == 'POST':
+        defect.delete()
+        messages.warning(request, "Defect Successfully Deleted!")
+        return redirect(reverse(view_defects))
+    
+    return render(request, 'products/confirm_delete.html', {
+        'delete': 'Defect',
+        'action': reverse(
+            delete_defect, args=[defect.id]
+        ),
+        'cancel': reverse(view_defects)
+    })
+
+
 
 # Create your views here.
