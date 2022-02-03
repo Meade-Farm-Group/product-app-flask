8106ed8194fe535c350b2d0e01ab9ad9d0884abd
Summersby95 <james.summersby@gmail.com>
2022-02-03 10:47:26 +0000

    feat: add outstanding tasks lookup template tag

--- npd/products/templatetags/lookup.py
+++ npd/products/templatetags/lookup.py
@@ -1,4 +1,6 @@
 from django.template.defaulttags import register
+from products.models import *
+
 
 @register.filter
 def lookup(object, key):
@@ -14,3 +16,50 @@ def get_type(value):
         return "list"
     else:
         return "str"
+
+
+@register.filter
+def is_viewer(user):
+    return user.groups.filter(name="Viewer").exists()
+
+
+@register.filter
+def outstanding_tasks(user, product):
+    checks = {
+        "Commercial": [
+            CommercialModel,
+            ApprovedOrigin,
+            ApprovedSupplier,
+        ],
+        "Packaging": [
+            InnerPackaging,
+            OuterPackaging,
+            Palletisation,
+        ],
+        "Operations": [
+            OperationsModel
+        ],
+        "Technical": [
+            DefectSpecification,
+            FinishedProduct,
+        ],
+        "Prophet": [
+            ProphetModel,
+        ]
+    }
+    outstanding_tasks = product.outstanding_checks()
+    user_group = user.groups.all().first().name
+    
+    user_outstanding = []
+
+    if user_group in outstanding_tasks:
+        for model in checks[user_group]:
+            done = False
+            if len(model.objects.filter(product=product)) > 0:
+                done = True
+            user_outstanding.append({
+                "name": model._meta.verbose_name.title(), 
+                "done": done,
+            })
+
+    return user_outstanding
